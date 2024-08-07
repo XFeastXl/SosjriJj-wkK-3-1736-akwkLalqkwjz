cde57bb1602352ccb1df8f99d4fc12665b112d74
XFeastXl <hakmkhalil@gmail.com>
2024-08-07 13:09:00 +0400

    Update and rename Whitelist.lua to whitelist.lua

--- Whitelist.lua
+++
@@ -1,8 +0,0 @@
-return {
-    players = {
-        [7054982083] = true,
-        [7074482563] = true,
-        [7043205418] = true,
-        -- Add more players here
-    }
-}
---
+++ whitelist.lua
@@ -0,0 +1,9 @@
+local whitelist = {
+    players = {
+        [12345678] = true, -- Replace with actual player IDs
+        [87654321] = true,
+        -- ...
+    }
+}
+
+return whitelist
